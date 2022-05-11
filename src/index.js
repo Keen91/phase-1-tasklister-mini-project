0c6b304393c298fc028bcffb33940039266829de
keen91 <kinan.hatahet@gmail.com>
2022-05-11 14:23:33 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+const form = document.getElementById("create-task-form");
+const task = document.querySelector("#new-task-description");
+const myToDos = document.querySelector("#tasks");
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    addTasks();
+    form.reset();
+  }); 
 });
+
+function addTasks() {
+  let li = document.createElement('li');
+  let button = document.createElement('button');
+
+  li.textContent = `${task.value} `
+  myToDos.appendChild(li);
+
+  button.textContent = 'x';
+  button.addEventListener('click', deleteTask);
+  myToDos.lastElementChild.appendChild(button);
+}
+
+function deleteTask(event) {
+  event.target.parentNode.remove();
+}
